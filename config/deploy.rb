fd84b67667af7c0c72b36f707183ed2e7860718b
root <root@dearboy01.(none)>
2014-04-23 03:22:12 -0400

    Configed Cap setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
+require "bundler/capistrano"
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'myapp'
+set :repo_url, 'git@github.com:dearboy15/myapp_server.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
@@ -14,13 +15,12 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
 # set :log_level, :debug
 
-# Default value for :pty is false
-# set :pty, true
+# Default value for :pty is false set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
